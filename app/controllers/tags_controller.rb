ab14e925665cf5729aa0dcb1de6e4f7b17ea549c
Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>
2021-03-17 10:28:50 -0700

    Add routes and controllers
    Co-authored-by: Geof Rosenmund <geof.rosenmunds.email@gmail.com>

---
+++ app/controllers/tags_controller.rb
@@ -0,0 +1,55 @@
+class TagsController < ApplicationController
+
+  def index
+    @tags = Tag.all
+    render :index
+  end
+
+  def new
+    @tag = Tag.new
+    render :new
+  end
+
+  def create
+    @tag = Tag.new(tag_params)
+    if @tag.save
+      flash[:notice] = "Tag successfully added!"
+      redirect_to tags_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @tag = Tag.find(params[:id])
+    render :edit
+  end
+
+  def show
+    @tag = Tag.find(params[:id])
+    render :show
+  end
+
+  def update
+    @tag= Tag.find(params[:id])
+    if @tag.update(tag_params)
+      flash[:notice] = "Tag successfully updated!"
+      redirect_to tags_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @tag = Tag.find(params[:id])
+    @tag.destroy
+    flash[:notice] = "Tag deleted!"
+    redirect_to tags_path
+  end
+
+  private
+    def tag_params
+      params.require(:tag).permit(:tag)
+    end
+
+end
